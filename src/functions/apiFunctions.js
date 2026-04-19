e74cf1c1df33c5eb24f224cc18febc9dd9b8a4bf
David Vasquez <davidvasquez@10-249-144-252.wireless.oregonstate.edu>
2026-04-19 16:54:01 -0700

    updates

--- src/functions/apiFunctions.js
+++ src/functions/apiFunctions.js
@@ -16,11 +16,19 @@ function getAPI() {
 
         return response;
       }, function (error) {
-        console.log(error.response.status)
-        console.log(error.response)
-        
+        const res = error.response;
+        if (res) {
+          console.log(res.status);
+          console.log(res);
+        } else {
+          console.log(
+            'INTERCEPTOR: no HTTP response (network/CORS/offline).',
+            error.message || error.code || error
+          );
+        }
+
         //STATUS 498:  Need to get a access token
-        if(error.response.status == 498) {
+        if (res && res.status === 498) {
           console.log("__________________________")
           console.log("INTERCEPTOR 498: We got a 498 so we need a new access token. Will send refresh token ")
           console.log("Status: 498")
@@ -28,21 +36,20 @@ function getAPI() {
           const refreshOutcome = functions.refreshToken();
           console.log("Message: We refreshed the access token!")
           console.log("__________________________")
-        } 
-      
+        }
+
         //STATUS 440: Logout User
-        if(error.response.status == 440) {
+        if (res && res.status === 440) {
           console.log("__________________________")
-          console.log("INTERCEPTOR 440: We got a 440 we couldn't refresh the access token because the refresh token was no good need to logout the user")  
+          console.log("INTERCEPTOR 440: We got a 440 we couldn't refresh the access token because the refresh token was no good need to logout the user")
           console.log("Status: 440")
           console.log("Message: Logout the user!")
-          localStorage.setItem('localStorageCurrentUser', JSON.stringify("null")); 
+          localStorage.setItem('localStorageCurrentUser', JSON.stringify("null"));
           window.location.href = '/login';
           console.log("__________________________")
         }
-        
+
         return Promise.reject(error);
-        
       });
 
       return axiosRequest;
